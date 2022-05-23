3316271e8678a174e5bec007255a1a6d48cfafac
Corey Brown <corey.brown.ny@gmail.com>
2022-05-23 15:58:44 -0400

    Added Home section

--- js/myScript.js
+++ js/myScript.js
@@ -1,4 +1,4 @@
-// When nav toggle button is pressed, nav list drops down
+// On mobile, When nav toggle button is pressed, nav list drops down
 
 const navToggleButton = document.getElementsByClassName("nav-toggler")[0];
 const navList = document.getElementsByClassName("nav-list")[0];
@@ -11,9 +11,23 @@ navToggleButton.addEventListener("click", () => {
 
 
 
-// Changes the active state of nav links when clicked
+// On mobile, when nav link is clicked, nav list goes away
 
 const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(navLink => {
+	navLink.addEventListener("click", function() {
+		navList.classList.toggle("display-nav-list");
+	});
+});
+
+
+
+
+
+// Changes the active state of nav links when clicked
+
+//const navLinks is declared above
 
 navLinks.forEach(navLink => {
 	navLink.addEventListener("click", function() {
